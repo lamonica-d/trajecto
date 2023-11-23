c26389b91cd54ec83d7faebdf4ff12167d4aefc5
Mishal Cohen <mishal.cohen@ifremer.fr>
2023-11-23 15:48:49 +0100

    factorisation of scripts

---
+++ trajecto_targets.R
@@ -0,0 +1,10 @@
+# integration de targets au research compendium 
+library(targets)
+library(tarchetypes)
+library(ggplot2)
+library(visNetwork)
+
+#this sources all the function to the target project
+tar_source()
+
+# prepare data
