0f35bd8ddf8c04b087de02de6470a21c536716f9
lamonica-d <dominique.lamonica@gmail.com>
2023-11-24 10:15:40 +0100

    add data + modifs scripts

--- R/script07_data_for_plots.R
+++ R/script07_data_for_plots.R
@@ -22,6 +22,13 @@ data_pour_standardiser=data.frame(data_pour_standardiser)
 moy_vect=c(data_pour_standardiser$moy_h,data_pour_standardiser$moy_v,data_pour_standardiser$moy_t)
 sd_vect=c(data_pour_standardiser$sd_h,data_pour_standardiser$sd_v,data_pour_standardiser$sd_t)
 
+numpoiss_vect0=c(3100, 3128, 3170, 3744 ,3121, 3183 ,3212 ,3240, 3352, 3464, 3730 ,3758 ,3632, 3786, 3835, 3849, 3856, 3870)
+
+id_sp=c("B-3100", "B-3128", "B-3170", "B-3744" ,
+        "C-3121", "C-3183" ,"C-3212" ,"C-3240", "C-3352", "C-3464", "C-3730" ,"C-3758" ,
+        "S-3632", "S-3786", "S-3835", "S-3849", "S-3856", "S-3870")
+
+vect_tailles=c(397,311,302,477,455,486,313,325,248,472,454,462,720,620,496,367,1001,1050)
 
 ##########################
 #####plots medianes ######
