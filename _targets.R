651f488c83189942506cb95fbccf4d3dbacfce0b
Mishal Cohen <mishal.cohen@ifremer.fr>
2023-11-24 12:18:40 +0100

    target

--- _targets.R
+++ _targets.R
@@ -27,24 +27,24 @@ list(
   #, tar_target(inf, inferences(data_for_inference))
 
   # declare the input data for script04
-  , tar_target(machin_files, c("outputs/image_inf_individual_3856_v10.Rdata", 
-                               "outputs/image_inf_individual_3856_v10.Rdata", 
-                               "outputs/image_inf_individual_3100_v10.Rdata"), 
-               format = "file")
+  # , tar_target(machin_files, c("outputs/image_inf_individual_3856_v10.Rdata", 
+  #                              "outputs/image_inf_individual_3856_v10.Rdata", 
+  #                              "outputs/image_inf_individual_3100_v10.Rdata"), 
+  #              format = "file")
   
   # concatenate inferences (script04)
-  , tar_target(mtot, treatment_posterior(machin_files))
-               
-  # parameter classification (script05)
-  , tar_target(classif1, classif_computation(mtot))
-  
-  # classification decision (script06)
-  , tar_target(classif2, classif_decision(classif1))
-  
-  # data frame generation for plots (script07)
-  , tar_target(plot, make_df_for_plot(mtot, classif1, classif2))
-  
-  # generate quarto file
-  , tar_render(report, "trajecto_targets.qmd")
+  # , tar_target(mtot, treatment_posterior(machin_files))
+  #              
+  # # parameter classification (script05)
+  # , tar_target(classif1, classif_computation(mtot))
+  # 
+  # # classification decision (script06)
+  # , tar_target(classif2, classif_decision(classif1))
+  # 
+  # # data frame generation for plots (script07)
+  # , tar_target(plot, make_df_for_plot(mtot, classif1, classif2))
+  # 
+  # # generate quarto file
+  # , tar_render(report, "trajecto_targets.qmd")
   
 )
