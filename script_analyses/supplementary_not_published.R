56e1efb5f9503fa46176c27ad89d73bf4c66fe0c
lamonica-d <dominique.lamonica@gmail.com>
2024-07-30 11:51:45 +0200

    reorganise draft/unpublished parts + gitignore update

--- script_analyses/supplementary_not_published.R
+++ script_analyses/supplementary_not_published.R
@@ -1,63 +1,216 @@
-
 #######################################################################################
 #######################################################################################
-# Script 2 - Data preparation - generating one file per individual usable for JAGS inference
+#  Generation of dataframes for heatmaps + plot heatmaps
 #######################################################################################
 #######################################################################################
 
-## fish id numbers
-numpoiss_vect=c(3100,3156,3744,3128,3170,3051,3317,3674,3828,3856,3632,3786,3835,3849,3870,3772,3800,3387,3415,3429,
-                3183,3212,3240,3352,3464,3730,3758,3121,3394,3408,3422,3506,3562,3590,3625,3079,3303)
-poiss=1:length(numpoiss_vect)
+## load and set data
+load("outputs/tables_list")
+load("outputs/clust_list")
+load("outputs/gp_list_list")
+load("outputs/nb_gp_list")
+load("outputs/matrice_curves_list")
+load("outputs/matrice_proba_curves_list")
+load("outputs/mtot_list")
 
-## loading unique .csv data file
-data8_sup2=read.table(file="data/derived-data/data_tous_individus_ready_to_use.csv", header=T, sep=",")[,-1]
+param_names=colnames(mtot_list[[1]])
+index_lin=3:8
+index_carre=9:14
+replicats=200
+nb_classe=c(4,2,4,2,2,3)
+xvalues_plot <- xvalues <- seq(-1.5,1.5,0.2)
 
-## vector of the number of trajectories per individual, to save
-nbsection=numeric(length(poiss))
-for (i in 1:length(poiss)){
-  temp1=data8_sup2[data8_sup2$numpoiss==i,]
-  nbsection[i]=length(unique(temp1$sqcont_new))
-}
-save(nbsection, file = paste0("data/derived-data/nb_section"))
+numpoiss_vect0=c(3100, 3128, 3170, 3744 ,3121, 3183 ,3212 ,3240, 3352, 3464, 3730 ,3758 ,3632, 3786, 3835, 3849, 3856, 3870)
+id_sp=c("B-3100", "B-3128", "B-3170", "B-3744" ,
+        "C-3121", "C-3183" ,"C-3212" ,"C-3240", "C-3352", "C-3464", "C-3730" ,"C-3758" ,
+        "S-3632", "S-3786", "S-3835", "S-3849", "S-3856", "S-3870")
+vect_tailles=c(397,311,302,477,455,486,313,325,248,472,454,462,720,620,496,367,1001,1050)
 
-## for all individual, index for the first and last row of each trajectory in the dataframe
-section=list()
-debut_section=list()
-fin_section=list()
-fin_poiss_sup2=sapply(poiss,function(s) max(which(data8_sup2$numpoiss==s)))
+varenviroxproba1=c("Water depth, p(R->M)","Water depth, p(M->R)","Flow velocity, p(R->M)","Flow velocity, p(M->R)",
+                   "Upstream temperature diff, p(R->M)","Upstream temperature diff, p(M->R)")
 
-temp1=data8_sup2[data8_sup2$numpoiss==1,]
-section[[1]]=unique(temp1$sqcont_new)
-debut_section[[1]]=sapply(section[[1]],function(s) min(which(temp1$sqcont_new==s)))
-fin_section[[1]]=sapply(section[[1]],function(s) max(which(temp1$sqcont_new==s)))
+## load mean and sd used for standardisation of environmental covariables 
+## to navigate between standardised and true values
+load("data/raw-data/data_pr_std")
+data_pour_standardiser=data.frame(data_pour_standardiser)
+moy_vect=c(data_pour_standardiser$moy_h,data_pour_standardiser$moy_v,data_pour_standardiser$moy_t)
+sd_vect=c(data_pour_standardiser$sd_h,data_pour_standardiser$sd_v,data_pour_standardiser$sd_t)
 
-for (i in 2:length(poiss)){
-  temp1=data8_sup2[data8_sup2$numpoiss==i,]
-  section[[i]]=unique(temp1$sqcont_new)
-  debut_section[[i]]=sapply(section[[i]],function(s) min(which(temp1$sqcont_new==s)))+fin_poiss_sup2[i-1]
-  fin_section[[i]]=sapply(section[[i]],function(s) max(which(temp1$sqcont_new==s)))+fin_poiss_sup2[i-1]
-}
+##dataframe pour heatmap
+mu=0
+sd=1
 
-## organise as two matrices, to save
-mat_debut=matrix(data=0, nrow=length(poiss), ncol=max(nbsection))
-mat_fin=matrix(data=0, nrow=length(poiss), ncol=max(nbsection))
-for (i in 1:length(poiss)){
-  mat_debut[i,]=c(unlist(debut_section[[i]]), rep(0,max(nbsection)-length(unlist(debut_section[[i]]))))
-  mat_fin[i,]=c(unlist(fin_section[[i]]), rep(0,max(nbsection)-length(unlist(fin_section[[i]]))))
-}
-save(mat_debut, file = paste0("data/derived-data/mat_debut"))
-save(mat_fin, file = paste0("data/derived-data/mat_fin"))
+# valeurs extremes rencontres par les individus
+varenviro_tous=read.table(file="data/raw-data/min_max_mean_var_enviro_tous_ind.csv"
+                          , header=T, sep=",")[,-1]
+min_ind=read.table(file="data/raw-data/min_var_enviro_tous_par_ind.csv"
+                   , header=T, sep=",")[,-1]
+max_ind=read.table(file="data/raw-data/max_var_enviro_tous_par_ind.csv"
+                   , header=T, sep=",")[,-1]
+mean_ind=read.table(file="data/raw-data/mean_var_enviro_tous_par_ind.csv"
+                    , header=T, sep=",")[,-1]
 
-## generating one file per individual of interest
-numpoiss_vect_int=c(3128,3100,3240,3183,3170,3121,3744,3632,3730,3212,3856,3352,
-                    3849,3835,3758,3786,3464,3870)
+#choix nyct period var nyct_i aube 8, crep 9, jour 10, nuit 11
+nyct_i=10
 
-for (k in 1:length(numpoiss_vect_int)){
+#si on veut la gamme de valeurs rencontrees par le poisson true_env=T
+true_env=F
+
+#nombre de cases de cote
+size_map=20
+df_heatmap=data.frame(individu=as.factor(id_sp[1]), env_x=as.vector(NA),env_y=as.vector(NA),prob=as.vector(NA)
+                      ,couple=as.factor("p(R->M)"))
+
+for (i in 1:18){
+  temp=mtot_list[[i]]
+  vect_a_p12=as.numeric()
+  vect_a_p21=as.numeric()
+  for (j in 17:length(temp[1,])){
+    param_quantile=as.numeric(quantile(temp[,j],probs = 0.5))
+    if (is.odd(j)==T) {vect_a_p12=c(vect_a_p12,param_quantile)}else{
+      vect_a_p21=c(vect_a_p21,param_quantile)
+    }
+  }
+  mat_coeff=matrix(data=c(vect_a_p12,vect_a_p21), ncol=2, byrow = F)
+  rm(temp)
   
-  id_poiss_int=numpoiss_vect_int[k]
-  data_ind=data8_sup2[data8_sup2$id_poiss==id_poiss_int,]
+  if (true_env==T){
+    dftemp=seq(min_ind[i,5],max_ind[i,5],length.out=size_map)
+    hmod=seq(min_ind[i,3],max_ind[i,3],length.out=size_map)
+    vmod=seq(min_ind[i,1],max_ind[i,1],length.out=size_map)
+    
+    mu_h=data_pour_standardiser$moy_h
+    mu_v=data_pour_standardiser$moy_v
+    mu_t=data_pour_standardiser$moy_t
+    
+    sd_h=data_pour_standardiser$sd_h
+    sd_v=data_pour_standardiser$sd_v
+    sd_t=data_pour_standardiser$sd_t
+    
+  }else{
+    
+    dftemp=seq(-2,2,length.out=size_map)
+    hmod=seq(-2,2,length.out=size_map)
+    vmod=seq(-2,2,length.out=size_map)
+    
+    mu_h=0
+    mu_v=0
+    mu_t=0
+    
+    sd_h=1
+    sd_v=1
+    sd_t=1
+  }
   
+  ###########calcul des proba et df################
+  
+  mat_vh=mesh(vmod,hmod)
+  p_vh=list()
+  df_vh=list()
+  for (j in 1:2){
+    p_vh[[j]]=with(mat_vh,(exp(mat_coeff[1,j] +mat_coeff[nyct_i,j] + mat_coeff[2,j] * ((y-mu_h)/sd_h) + mat_coeff[3,j] * ((x-mu_v)/sd_v) + mat_coeff[5,j] * ((y-mu_h)/sd_h)^2 + mat_coeff[6,j] * ((x-mu_v)/sd_v)^2)/
+                             (1 +  exp(mat_coeff[1,j] +mat_coeff[nyct_i,j] + mat_coeff[2,j] * ((y-mu_h)/sd_h) + mat_coeff[3,j] * ((x-mu_v)/sd_v) + mat_coeff[5,j] * ((y-mu_h)/sd_h)^2 + mat_coeff[6,j] * ((x-mu_v)/sd_v)^2)))
+    )
+    #x v 3,,6
+    #y h 2,,5
+    if (j==1){couple_nom="p(R->M)"}else{couple_nom="p(M->R)"}
+    df_vh[[j]]=data.frame(individu=as.factor(rep(id_sp[i],length(as.vector(t(p_vh[[j]]))))), env_x=as.vector(t(mat_vh[[1]])),env_y=as.vector(t(mat_vh[[2]])),prob=as.vector(t(p_vh[[j]]))
+                          ,couple=as.factor(rep(couple_nom,length(as.vector(t(p_vh[[j]]))))))
+  }
+  
+  #un seul df
+  df_heatmap=rbind(df_heatmap,df_vh[[1]],df_vh[[2]])
+  #df_heatmap=df_heatmap[-1,]
+}#i
+
+save(df_heatmap, file = "outputs/df_ggplot_heatmap_all_values")
+
+
+#############version avec que les valeurs rencontrees##########################
+
+data1=read.table(file="data/derived-data/data_tous_individus_ready_to_use_v4.csv", header=T, sep=",")[,-1]
+
+#choix nyct period var nyct_i aube 8, crep 9, jour 10, nuit 11
+nyct_i=10
+
+########prepa du df######
+df_heatmap2=data.frame(individu=as.factor(id_sp[1]), env_x=as.vector(NA),env_y=as.vector(NA),
+                       env_x_tv=as.vector(NA),env_y_tv=as.vector(NA), prob=as.vector(NA)
+                       ,couple=as.factor("p(R->M)"))
+for (i in 1:18){
+  
+  #les param
+  temp=mtot_list[[i]]
+  vect_a_p12=as.numeric()
+  vect_a_p21=as.numeric()
+  for (j in 17:length(temp[1,])){
+    param_quantile=as.numeric(quantile(temp[,j],probs = 0.5))
+    if (is.odd(j)==T) {vect_a_p12=c(vect_a_p12,param_quantile)}else{
+      vect_a_p21=c(vect_a_p21,param_quantile)
+    }
+  }
+  mat_coeff=matrix(data=c(vect_a_p12,vect_a_p21), ncol=2, byrow = F)
+  rm(temp)
+  
+  #l'enviro
+  enviro=data1[data1$id_poiss==numpoiss_vect0[i],c(1,9,10)]
+  enviro_non_std=data.frame(vmod_tv=enviro$vmod*data_pour_standardiser$sd_v+data_pour_standardiser$moy_v,
+                            hmod_tv=enviro$hmod*data_pour_standardiser$sd_h+data_pour_standardiser$moy_h)
+  
+  ###########calcul des proba et df################
+  df_vh=list()
+  
+  for (j in 1:2){
+    
+    p_vh1=as.numeric()
+    
+    for (e in 1:length(enviro[,1])){
+      
+      p_vh1[e]=exp(mat_coeff[1,j] +mat_coeff[nyct_i,j] + mat_coeff[2,j] * enviro[e,3] + mat_coeff[3,j] * enviro[e,2] + mat_coeff[5,j] * enviro[e,3]^2 + mat_coeff[6,j] * enviro[e,2]^2)/
+        (1 +  exp(mat_coeff[1,j] +mat_coeff[nyct_i,j] + mat_coeff[2,j] * enviro[e,3] + mat_coeff[3,j] * enviro[e,2] + mat_coeff[5,j] * enviro[e,3]^2 + mat_coeff[6,j] * enviro[e,2]^2))
+      #x v 3,,6
+      #y h 2,,5
+    }#e
+    
+    
+    if (j==1){couple_nom="p(R->M)"}else{couple_nom="p(M->R)"}
+    df_vh[[j]]=data.frame(individu=as.factor(rep(id_sp[i],length(p_vh1))), env_x=as.vector(enviro$vmod),env_y=as.vector(enviro$hmod),
+                          env_x_tv=as.vector(enviro_non_std$vmod_tv),env_y_tv=as.vector(enviro_non_std$hmod_tv),
+                          prob=as.vector(p_vh1) ,couple=as.factor(rep(couple_nom,length(p_vh1)))
+    )
+  }#j
+  
+  #un seul df
+  df_heatmap2=rbind(df_heatmap2,df_vh[[1]],df_vh[[2]])
+  
+}#i  
+
+save(df_heatmap2, file = "outputs/df_ggplot_heatmap_encountered_values")
+
+
+
+#######################################################################################
+#######################################################################################
+load(file="outputs/df_ggplot_coeff_nyct")
+#################anova test################
+library(rstatix)
+#pour prm
+for (i in 1:length(id_sp)){
+  temp=df_coeff_nyct[df_coeff_nyct$individu==id_sp[i] & df_coeff_nyct$proba=="p(R->M)",]
+  print(id_sp[i])
+  print(anova_test(temp, yvalues_coeff~ nyct))
+  print(tukey_hsd(temp, yvalues_coeff~ nyct))
+}
+
+#pour pmr
+for (i in 1:length(id_sp)){
+  temp=df_coeff_nyct[df_coeff_nyct$individu==id_sp[i]  & df_coeff_nyct$proba=="p(M->R)",]
+  print(id_sp[i])
+  print(anova_test(temp, yvalues_coeff~ nyct))
+  print(tukey_hsd(temp, yvalues_coeff~ nyct))
+}
+
+
   ##################interpolating NAs in environmental covariables#######################
   #sil
   if (id_poiss_int==3786){data_ind$temp[1315]=data_ind$temp[1314]
@@ -193,13 +346,3 @@ for (k in 1:length(numpoiss_vect_int)){
     data_ind$vmod[i]<-(data_ind$vmod[i-1]+data_ind$vmod[i+1])/2
   }
   
-  # sum(is.na(data_ind$vmod))
-  # sum(is.na(data_ind$hmod))
-  # sum(is.na(data_ind$temp))
-  
-  save(data_ind, file = paste0("data/derived-data/data_ind",id_poiss_int,sep=""))
-  
-}
-
-
-
